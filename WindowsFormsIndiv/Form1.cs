589af5ac182674a9ba1ee9cc9ed6ac6119c9a679
Minaylov <012_17@2310018058.onmicrosoft.com>
2021-09-08 21:06:53 +0300

    Init

---
+++ WindowsFormsIndiv/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsIndiv
+{
+    public partial class Form1 : Form
+        //Здравствуйте!Данную работу выполнил Минайлов Олег Студент 12 группы 
+        //Данная программа(код) является шифратором,он шифрует некоторые символы,буквы и цифры,можно вводить данные
+        //как с клавиатуры ,так и с помощью текстового файла
+    {
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            bool ValidCharFound(string str)//функция проверки наличия только символовцифр и , в //строке
+            {
+                bool valid = true;// возвращаемый результат
+                foreach (char c in str) // искать символ c в строке str
+                {
+                    string bfr = c.ToString();// конвертировать с в строку
+                    if (Regex.IsMatch(bfr, @"[0-9,]"))//если в строке только
+                    {
+                        valid = true;//то результат=истина
+                    }
+                    else
+                    {
+                        valid = false;//иначе ложь
+                        break;
+                    }
+                }
+                return valid;
+            }
+            if (textBox3.Text != null && textBox3.Text != "")
+            {
+                if (ValidCharFound(textBox3.Text))    //проверка на правильность 
+                                                      //введенных данных
+                { }
+                else
+                    MessageBox.Show("Введите ключ шифрования(он состоит из цифер)", "Ошибка", MessageBoxButtons.OK,
+                  MessageBoxIcon.Error);
+                
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); //кнопка выхода из программы
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)  //Диалоговое окно с загрузкой данных
+            {
+                Name = openFileDialog1.FileName;
+                textBox1.Clear();
+                textBox1.Text = File.ReadAllText(Name);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Удаление данных
+            MessageBox.Show("Данные были удалены", "Удаление данных", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)  //Диалоговое окно с сохранением данных
+            {
+                Name = saveFileDialog1.FileName;
+                File.WriteAllText(Name, textBox2.Text);
+            }
+        }
+
+        static string ABC = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ0123456789?!()-"; //Наши используемые буквы,цифры и символы
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            textBox2.Text = ooo(textBox1.Text);
+        }
+        public string ooo(string inp)
+        {
+            StringBuilder cipher = new StringBuilder();
+
+            string a = textBox1.Text;          //Шифрование кода
+            int b = 1;
+
+            for (int i = 0; i < a.Length; i++)
+                for (int e = 0; e < ABC.Length; e++)
+                    if (a[i] == ABC[e]) cipher.Append(ABC[(e + b + ABC.Length) % ABC.Length]);
+            textBox1.Clear();
+            return cipher.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            {
+                textBox1.Text = ooo0(textBox2.Text);
+            }
+             string ooo0(string inp)
+            {
+
+                StringBuilder code = new StringBuilder();
+
+                string a = textBox2.Text;
+                int b = 8;
+                for (int i = 0; i < a.Length; i++)
+                    for (int x=0; x < ABC.Length; x++)
+                        if (a[i] == ABC[x]) code.Append(ABC[(x - b + ABC.Length) % ABC.Length]);
+
+                textBox2.Clear();
+                return code.ToString();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        { 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormHelp f2 = new FormHelp();        //Возможность нажимать на кнопки с выводом доп.окон
+            f2.Show();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            FormHints f3 = new FormHints();   ////Возможность нажимать на кнопки с выводом доп.окон
+            f3.Show();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            FormAnswerOnOurQuestions f4 = new FormAnswerOnOurQuestions();  //Возможность нажимать на кнопки с выводом доп.окон
+            f4.Show();
+        }
+    }
+}
---
+++ WindowsFormsIndiv/FormAnswerOnOurQuestions.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsIndiv
+{
+    partial class FormAnswerOnOurQuestions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnswerOnOurQuestions));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(78, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ответы на вопросы";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(33, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(321, 75);
+            this.label2.TabIndex = 1;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(33, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(337, 90);
+            this.label3.TabIndex = 2;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(33, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(315, 60);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Если у вас есть какие-то вопросы,\r\nобращайтесь к разработчику и в последствии \r\nо" +
+    "н добавит сюда ваши вопросы с ответами на них 8)\r\n\r\n";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Location = new System.Drawing.Point(121, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 36);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Закрыть";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormAnswerOnOurQuestions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(377, 346);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormAnswerOnOurQuestions";
+            this.Text = "Ответы на вопросы";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
